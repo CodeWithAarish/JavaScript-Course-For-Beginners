f07b24e223981dca51b92936797795d214375c44
CodeWithAarish <mohammedaarish240@gmail.com>
2024-01-15 18:44:25 +0530

    Tutorial 2

---
+++ Tutorial 2/script.js
@@ -0,0 +1,7 @@
+console.log("This is the code for the ultimate javascript course") // This is the valid syntax for printing
+// console.tullu("This is the code for the ultimate javascript course") // This is an invalid syntax
+
+let a = 76 // a contains 76
+console.log(a)
+a = "aarish"
+console.log(a)
