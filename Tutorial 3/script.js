9e503e22f43b985f3d6887c43050bf22a73a1bff
CodeWithAarish <mohammedaarish240@gmail.com>
2024-01-17 15:48:03 +0530

    Tutorial 3

---
+++ Tutorial 3/script.js
@@ -0,0 +1,15 @@
+console.log("JavaScript tutorial 3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "Aarish";
+const author = "Aarish"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const aarish = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
