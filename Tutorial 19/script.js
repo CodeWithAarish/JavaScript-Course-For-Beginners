578c0ac91eface3e61bfbdd6031144586b3d8a7d
CodeWithAarish <mohammedaarish240@gmail.com>
2024-02-13 19:14:36 +0530

    Tutorial 19

---
+++ Tutorial 19/script.js
@@ -0,0 +1,24 @@
+let num = [3, 5, 1, 2, 4]
+
+// for (let i = 0; i < num.length; i++) {
+//     console.log(num[i])
+// }
+
+// For Each Loop
+// num.forEach((element) => {
+//     console.log(element * element)
+// })
+
+// Array.from
+let name = "Aarish";
+let arr = Array.from(name)
+console.log(arr)
+
+// For of
+// for(let i of num){
+//     console.log(i)
+// }
+
+// For in
+for(let k in num)
+console.log(num[k])
